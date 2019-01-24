eb1c393f026a7efc7246a14faf967a418067f84f
wesley santana <wesleysantana@gmail.com>
2019-01-24 12:42:10 -0200

    Primeiro Commit do projeto, Criacao da estrura da Api em Java

---
+++ src/main/java/br/com/brasilseg/emissao/EmissaoApplication.java
@@ -0,0 +1,15 @@
+package br.com.brasilseg.emissao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EmissaoApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EmissaoApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/brasilseg/cursomv/GovernanciaApiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.brasilseg.cursomv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GovernanciaApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
